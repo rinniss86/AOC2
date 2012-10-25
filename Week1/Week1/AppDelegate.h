fe0b3bc5785e7fc1551fe1e958d83b6425f57c9f
rinniss86 <rinniss86@gmail.com>
2012-10-24 20:25:43 -0400

    Initial Files added.

---
+++ Week1/Week1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Week1
+//
+//  Created by Richard Inniss on 10/24/12.
+//  Copyright (c) 2012 Richard Inniss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Week1/Week1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Week1
+//
+//  Created by Richard Inniss on 10/24/12.
+//  Copyright (c) 2012 Richard Inniss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
