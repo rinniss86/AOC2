f702631d06bdf254178cd6712b9208e004723b4e
rinniss86 <rinniss86@gmail.com>
2012-12-06 00:34:12 -0500

    Added Week 3 Files.

---
+++ Week3/week3/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  week3
+//
+//  Created by Richard Inniss on 12/6/12.
+//  Copyright (c) 2012 Richard Inniss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Week3/week3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  week3
+//
+//  Created by Richard Inniss on 12/6/12.
+//  Copyright (c) 2012 Richard Inniss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
